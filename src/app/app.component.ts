8728b06f6846e352e3f8d235a2ebbfa1b203beaf
Luke Bertram <lukebertram@gmail.com>
2018-01-19 09:44:46 -0800

    Add masterAnimalList array to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal }    from './animal.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Zoo Lord';
+  selectedAnimal: Animal = null;
+
+  masterAnimalList: Animal[] = [
+    new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", ["Cool shade"], ["Loud noises"]),
+    new Animal("Ocelot", "Revolver", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", ["Laying in the sunshine", "Gun tricks"], ["Toys that are not rope-based"]),
+    new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Female", ["Delicate roots and leaves"], ["Loud noises"])
+  ];
 }
